8d4b4a7654f862d0d72f53235ec8e7d7c3704289
iknox <iknox@narrativescience.com>
2019-12-17 15:07:44 -0600

    ShellCheck wants quotes

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ WAR_CHECKSUM='2a8f906235fac6329dc8e9b143e291365357d9b9'
 LOCAL_WAR_CHECKSUM=$(shasum DataSync2019/www/ROOT.war | cut -f1 -d' ')
 
 
-if [ $LOCAL_WAR_CHECKSUM != $WAR_CHECKSUM ]; then
+if [ "$LOCAL_WAR_CHECKSUM" != "$WAR_CHECKSUM" ]; then
   #debug
   # echo "|${LOCAL_WAR_CHECKSUM}| != |${WAR_CHECKSUM}|"
   echo "Slow your roll, it looks like your ROOT.war file is the wrong size!"
